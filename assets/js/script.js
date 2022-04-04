7c296a129ebcdd2d9cba7a046df61643dc3c4afd
Nigel Doheny <nigel.doheny93@gmail.com>
2022-04-04 12:11:25 +0000

    add JS structure

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+function incrememntWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+} 
+
+function displayMultiplyQuestion(){
+
+} 
