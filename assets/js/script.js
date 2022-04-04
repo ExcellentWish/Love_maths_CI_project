65d3488b2f8cc8ab42e923c78c2c8ee43ba7b7c1
Nigel Doheny <nigel.doheny93@gmail.com>
2022-04-04 15:53:49 +0000

    add subtract code

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,9 @@ function runGame(gameType){
       displayAdditionQuestion(num1, num2);
   } else if(gameType === "multiply"){
     displayMultiplyQuestion(num1, num2);
-    } else {
+    } else if (gameType === "subtract"){
+        displaySubtractQuestion(num1, num2);
+    }  else {
       alert(`Unkown game type: ${gameType}`)
     throw  `Unkown game type: ${gameType}. Aborting!`;
   }
@@ -70,6 +72,8 @@ if ( operator === "+"){
     return [ operand1 + operand2, "addition"];
 } else if (operator ==="x"){
     return[operand1 * operand2, "multiply"];
+}else if (operator === "-"){
+    return[operand1-operand2, "subtract"];
 }else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemnted operator ${operator}. Aborthing!`;
@@ -101,6 +105,9 @@ function displayAdditionQuestion(operand1, operand2){
 
 function displaySubtractQuestion(operand1, operand2){
     
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-"; 
 
 } 
 
